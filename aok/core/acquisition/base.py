aea6d7f4c2ccea175be157de04a5c665c43c57ed
hhollandmoritz <hhollandmoritz@gmail.com>
2026-05-06 10:26:02 -0400

    cleaning up linter mess

--- aok/core/acquisition/base.py
+++ aok/core/acquisition/base.py
@@ -5,12 +5,9 @@ from typing import Literal
 import warnings
 
 import pandas as pd
-<<<<<<< Updated upstream
-=======
 import geopandas as gpd
 from sliderule import sliderule, icesat2 
-from aok.core.kd_utils.data_processing import isolate_sea_land_photons
->>>>>>> Stashed changes
+from aok.core.kd_utils.data_processing import 
 
 OutputType = Literal["dataframe", "geodataframe", "files"]
 
