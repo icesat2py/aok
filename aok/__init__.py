a10a575e479a46cff2dd603fef8712c5b1e83c64
Jessica Scheick <jbscheick@gmail.com>
2026-05-21 12:08:04 -0400

    update init files

--- aok/__init__.py
+++ aok/__init__.py
@@ -1,8 +1,4 @@
+from _aok_version import version as __version__
+
 from .core import kd_utils
-
-# from _aok_version import version as __version__
-
-# import aok.core.kd_utils as kd_utils
-# from icepyx.core.read import Read
-# from icepyx.core.variables import Variables
-# from icepyx.quest.quest import Quest
+from .core import acquisition
--- aok/core/acquisition/__init__.py
+++ aok/core/acquisition/__init__.py
@@ -1,15 +1,1 @@
-"""
-the below code was causing unit test issues. probably due to icepyx_download not smoothly loading. will fix later.
-"""
-
-"""
-from .icepyx_download import icepyx_download
-from .slide_rule import sliderule_download
-from .request_data import request_data
-
-__all__ = [
-    "icepyx_download",
-    "sliderule_download",
-   # "request_data",
-]
-"""
+# Acquisition module for data acquisition from various sources
